a449796620d3b1ceb8e12ad098dc3304bc021ab0
KarlKaasan <Karl.Kaasan@tktk.ee>
2024-05-20 12:42:05 +0300

    new app

---
+++ Week5LogicalOperaton/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//AND - &&
+
+//true && true --- true
+//true & false --- false
+// false && true ---false
+//flse && false ---false
+
+// OR - (pipes)
+
+//true (pipes) true --- true
+//true (pipes) false --- true
+//false (pipes) true --- true
+//false (pipes) false --- false
